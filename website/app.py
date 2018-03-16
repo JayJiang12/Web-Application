133604b600f9ba3bfa553a8aca74772d7ed6750c
rocks6 <stevebyerly62@gmail.com>
2018-03-16 13:08:56 -0400

    fix python and ajax comms, update apache server config with proxy pass in order to allow flask and apache to play nice together

--- website/app.py
+++ website/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, json
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/getPoints", methods=["POST"])
-def getPoints():
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/getPoints', methods=["POST"])
+def getPoints():
     return json.dumps({"status" : "OK"})
 
 if __name__ == "__main__":
