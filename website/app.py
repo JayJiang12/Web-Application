0a29b52b71549b8f5fffd4f68a68226324d0ae7f
Steve B <stevebyerly62@gmail.com>
2018-03-22 18:45:29 -0400

    work on middle end

--- website/app.py
+++ website/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, json
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/getPoints", methods=["POST"])
-def getPoints():
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    return json.dumps({"status" : "OK"})
+@app.route('/getPoints', methods=["POST"])
+def getPoints():
+	jsdata = request.form['param']
+	return json.dumps(jsdata);
+#    return json.dumps([[39.260261, -76.711508], [38.9307318,-77.0080818,15]])
 
 if __name__ == "__main__":
 	app.run()
