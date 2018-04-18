b16310b42555f2087032045ab50bc93b39efb5e6
rocks6 <stevebyerly62@gmail.com>
2018-04-17 23:15:37 -0400

    begin frontend/backend integration

--- website/app.py
+++ website/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, json
 from flask import render_template
-import vesta
+#import vesta
 
 app = Flask(__name__)
 
@@ -10,8 +10,11 @@ def index():
 
 @app.route('/getPoints', methods=["POST"])
 def getPoints():
-    jsdata = request.form['param']
-    return json.dumps(jsdata);
+    col_slider = request.form['param_col_slider']
+    temp_min = request.form['param_temp_min']
+    temp_max = request.form['param_temp_max']
+    
+    return json.dumps(col_slider);
 #    return json.dumps([[39.260261, -76.711508], [38.9307318,-77.0080818,15]])
 
 if __name__ == "__main__":
